c0b701b57b3128c1885ed2a5e5a5b38489dd4f35
Никитин Роман <r.nikitin@maximaster.ru>
2025-05-16 21:34:36 +0300

    feat(config): Добавить featureSlice конфиг
    Добавлены плагины:
    - eslint-plugin-boundaries
    - eslint-plugin-import

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import antfu from '@antfu/eslint-config'
 import cssPlugin from '@eslint/css'
+import { feautureSlice } from './feautureSlice'
 import { javascript } from './javascript'
 
-export default antfu({ plugins: { css: cssPlugin } }).override('antfu/javascript/rules', javascript)
+export default antfu({ plugins: { css: cssPlugin } }, feautureSlice)
+  .override('antfu/javascript/rules', javascript)
