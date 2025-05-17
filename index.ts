8afe1a1650921df526a77c97b8b3a3b117874ad6
Богдан Биницкий <bbinitskiy@ibs.ru>
2025-05-18 00:43:18 +0300

    feat(config): Add Prettier and set up ESLint plugin

--- index.ts
+++ index.ts
@@ -2,9 +2,16 @@ import type { ConfigNames, TypedFlatConfigItem } from '@antfu/eslint-config'
 import type { FlatConfigComposer } from 'eslint-flat-config-utils'
 import antfu from '@antfu/eslint-config'
 import cssPlugin from '@eslint/css'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import { featureSliced } from './featureSliced'
 import { javascript } from './javascript'
 
-const config: FlatConfigComposer<TypedFlatConfigItem, ConfigNames> = antfu().append({ name: 'css-plugin', plugins: { css: cssPlugin } }).append(featureSliced).override('antfu/javascript/rules', javascript).remove('antfu/perfectionist/setup')
+const config: FlatConfigComposer<TypedFlatConfigItem, ConfigNames> = antfu()
+  .remove('antfu/perfectionist/setup')
+  .remove('antfu/stylistic/rules')
+  .override('antfu/javascript/rules', javascript)
+  .append(cssPlugin.configs.recommended)
+  .append(featureSliced)
+  .append(eslintPluginPrettierRecommended)
 
 export default config
