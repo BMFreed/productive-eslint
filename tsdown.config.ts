dda34a45ae71544e178beec893bc6fa5a58762d6
Богдан Биницкий <bbinickiy@liveai.ru>
2025-12-26 19:37:39 +0300

    fix(build): fix library not loading

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,5 +4,10 @@ export default defineConfig({
   entry: ['./src/index.config.ts'],
   format: ['esm'],
   minify: true,
-  shims: true,
+  shims: false,
+  cjsDefault: false,
+  outExtensions: () => ({
+    js: '.js',
+    dts: '.d.ts',
+  }),
 })
