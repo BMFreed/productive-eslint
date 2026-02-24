7d22806371239bb1135d029716eb0e6e27c02661
Биницкий Богдан <bbinickiy@liveai.ru>
2026-02-24 20:06:15 +0300

    fix(build): remove transitive external deps from build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'tsdown'
 
+/**
+ * Packages used by our plugin (productive) and by @antfu/eslint-config; keep
+ * external to avoid bundling and version drift.
+ */
+const externalDeps = [
+  '@eslint-community/eslint-utils',
+  '@typescript-eslint/scope-manager',
+  '@typescript-eslint/types',
+  '@typescript-eslint/utils',
+  '@typescript-eslint/visitor-keys',
+  'eslint-visitor-keys',
+]
+
 export default defineConfig({
   cjsDefault: false,
   entry: ['./src/index.config.ts'],
+  external: externalDeps,
   format: ['esm'],
   minify: true,
   outExtensions: () => ({
